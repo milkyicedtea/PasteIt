0b602afa5e7997df02ebefd8cc41620c9b55cd04
Cheek <81954613+milkyicedtea@users.noreply.github.com>
2026-05-25 14:19:58 +0200

    footer link pointed to the right page

--- src/client/components/Footer.tsx
+++ src/client/components/Footer.tsx
@@ -4,7 +4,7 @@ import { Link } from "@tanstack/react-router"
 export function Footer() {
   return (
     <Box className="flex items-center h-full">
-      <Text component={Link} to={'/'}>© 2025 051205.xyz</Text>
+      <Text component={Link} to={'https://051205.xyz'}>© 2025 051205.xyz</Text>
     </Box>
   )
 }
